6e9d5cec819dd3d694ae4bdbe4163129c3d22c14
Daniel Sousa <daniel.sousa@msn.com>
2016-12-28 16:36:49 -0200

    meu segundo commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8">        
+    <meta name="robots" content="index, follow">
+    <meta name="author" content="Daniel S. Costa" />
+    <meta name="description" content="">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    // put your code here
+    ?>
+  </body>
+</html>
